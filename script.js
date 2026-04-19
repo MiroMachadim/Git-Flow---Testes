c5b2c695fcc56b464357692f0cf206f98c201ec0
MiroMachadim <miromachadodnv@gmail.com>
2026-04-18 23:59:12 -0300

    javascript feito

---
+++ script.js
@@ -0,0 +1,29 @@
+function senhaValida(senha) {
+  if (senha.length < 4) {
+    return false;
+  } else {
+    return true;
+  }
+}
+function cpfValido(cpf) {
+  if (cpf.length < 11) {
+    return false;
+  } else {
+    let regex = /\d/;
+    if (cpf.regex.test(cpf)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+function formulario() {
+  let form = document.getElementById("formulario");
+  if (!senhaValida(form[1])) {
+    alert("senha invalidá!");
+  } else if (!cpfValido(form[2])) {
+    alert("cpf inválido! Use apenas digitos");
+  } else if (!senhaValida(form[3])) {
+    alert("nome inválido!");
+  } else alert("Cadastrado com sucesso!");
+}
